18572d52af5646296833e4d7640953c040519b4b
Shahzeb999 <57065715+Shahzeb999@users.noreply.github.com>
2024-01-08 14:35:25 +0530

    Update scipt.js

--- scipt.js
+++ scipt.js
@@ -79,7 +79,8 @@ document.getElementById('numberOfTeachers').addEventListener('input', function (
     for (let i = 0; i < NoOfTeachers; i++) {
         const div = document.createElement("div");
         div.id = `Teacher${i+1}`;
-        div.innerHTML = `<h3>Teacher ${i+1}: ${ans[i].sort()}</h3>`;
+        ans[i].sort();
+        div.innerHTML = `<h3>Teacher ${i+1}: ${ans[i]}</h3>`;
         disributed.append(div);
         console.log(div);
     }
